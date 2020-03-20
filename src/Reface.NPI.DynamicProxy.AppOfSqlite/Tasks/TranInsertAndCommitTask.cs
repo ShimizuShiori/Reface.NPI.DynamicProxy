264855e1d4fdf5aa43f32ce96226ca06b865349d
ShimizuShiori <116971263@qq.com>
2020-03-20 15:17:11 +0800

    添加事务提交的任务

---
+++ src/Reface.NPI.DynamicProxy.AppOfSqlite/Tasks/TranInsertAndCommitTask.cs
@@ -0,0 +1,28 @@
+﻿using Reface.NPI.DynamicProxy.AppOfSqlite.Daos;
+using Reface.NPI.DynamicProxy.AppOfSqlite.Entities;
+using System.Collections.Generic;
+
+namespace Reface.NPI.DynamicProxy.AppOfSqlite.Tasks
+{
+    public class TranInsertAndCommitTask : ITask
+    {
+        public string TaskName => "事务提交任务";
+
+        public void DoTask(IUserDao userDao, Dictionary<string, object> context)
+        {
+            DbConnectionContext ctx = (DbConnectionContext)context[Constant.CONTEXT_KEY_DB_CONTEXT];
+
+            ctx.BeginTran();
+            User user = User.New();
+            userDao.Insert(user);
+            ctx.Commit();
+
+            User user2 = userDao.SelectById(user.Id);
+            Checker.IsNotNull(user2);
+            Checker.Equals(user.Id, user2.Id);
+            Checker.Equals(user.Name, user2.Name);
+            Checker.Equals(user.LoginName, user2.LoginName);
+            Checker.Equals(user.Password, user2.Password);
+        }
+    }
+}
